94af1f1143ae8f0c3380cd14c93feb541ddf8a57
Lanchuii <74664641+Lanchuii@users.noreply.github.com>
2023-11-25 22:13:49 +0800

    added docker-compose and db config for prod and dev databases

--- config/db.ts
+++ config/db.ts
@@ -3,3 +3,17 @@ import mongoose from 'mongoose';
 export const connectToDatabase = (mongodbURL: string) => {
   return mongoose.connect(mongodbURL);
 };
+
+interface DbConfig {
+  databaseUrl: string;
+}
+
+const getDbConfig = (): DbConfig => {
+  return {
+    databaseUrl: process.env.NODE_ENV === 'production'
+      ? process.env.MONGODB_URL || ''
+      : process.env.DEV_MONGODB_URL || 'mongodb://localhost:27017/ws-db',
+  };
+};
+
+export default getDbConfig;
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { configureApp } from '../config/app';
-import { connectToDatabase } from '../config/db';
+import getDbConfig, { connectToDatabase } from '../config/db';
 
 dotenv.config();
-
 const app = express();
-const mongodbURL: string = process.env.MONGODB_URL || '';
+
+const dbConfig = getDbConfig();
+const mongodbURL = dbConfig.databaseUrl;
 const PORT = process.env.PORT || 3000
 
 configureApp(app)
@@ -16,6 +17,7 @@ connectToDatabase(mongodbURL)
     console.log('App connected to database');
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
+      console.log(`App running on ${process.env.NODE_ENV}`);
     });
   })
   .catch((error) => {
