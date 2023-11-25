418befb3564742371948d6b3928cc54f6e790f47
Lanchuii <74664641+Lanchuii@users.noreply.github.com>
2023-11-25 23:16:55 +0800

    Remove dist from version control

--- dist/server/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.URL = exports.PORT = void 0;
-exports.PORT = 3000;
-exports.URL = 'mongodb+srv://tllcc-user:GWkdtPI5qx5AYvAO@tllcc-cluster.7vxagsz.mongodb.net/schedule-collection?retryWrites=true&w=majority';
