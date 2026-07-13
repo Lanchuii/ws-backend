87c720de93fdc6f90ba5f816f503b3ef6f6cda05
Lanchuii <74664641+Lanchuii@users.noreply.github.com>
2026-07-14 01:56:49 +0800

    Fix schedule crypto runtime on Render

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { webcrypto } from 'crypto';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+
+if (!globalThis.crypto) {
+  Object.defineProperty(globalThis, 'crypto', {
+    value: webcrypto,
+  });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
