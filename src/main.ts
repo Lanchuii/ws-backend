8fdbfd249202926cc9fdc21d842e7cd09160cea4
Lanchuii <74664641+Lanchuii@users.noreply.github.com>
2026-07-14 01:39:16 +0800

    Fix Render backend start command

--- src/main.ts
+++ src/main.ts
@@ -26,8 +26,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
-  console.log(`App listening on: ${3000}`)
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`App listening on: ${port}`)
 }
 bootstrap();
 
